99a0e0d1ada3dee6b87ec3369d5e44475599ac52
Santo Pfingsten <core@lusito.info>
2024-06-02 00:03:03 +0200

    A way to prevent navigation. Resolves #3

--- packages/router/src/Router.tsx
+++ packages/router/src/Router.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState, useMemo, PropsWithChildren, FC } from "react";
 
 import { RouterContext, RouterContextValue } from "./RouterContext";
-import { createHistory, getHashPath } from "./history";
+import { createHistory, getHashPath, OnNavigateFn } from "./history";
 import { createRouteMatcher, RouteMatcherFactory } from "./routeMatcher";
 import { getPathWithoutBasename } from "./basename";
 import { simpleRouteMatcherFactory } from "./simpleRouteMatcherFactory";
@@ -10,6 +10,11 @@ export interface RouterProps {
     basename?: string;
     mode?: "hash" | "path";
     routeMatcherFactory?: RouteMatcherFactory;
+    /**
+     * An async callback that will be called on a navigation event.
+     * If it resolves to false, the navigation event will be aborted
+     */
+    onNavigate?: OnNavigateFn;
 }
 
 export const Router: FC<PropsWithChildren<RouterProps>> = ({
@@ -17,10 +22,14 @@ export const Router: FC<PropsWithChildren<RouterProps>> = ({
     mode,
     routeMatcherFactory = simpleRouteMatcherFactory,
     children,
+    onNavigate,
 }) => {
     const hashMode = mode === "hash";
     const [path, setPath] = useState(() => (hashMode ? getHashPath() : getPathWithoutBasename(basename)));
-    const history = useMemo(() => createHistory(hashMode, basename, setPath), [setPath, basename, hashMode]);
+    const history = useMemo(
+        () => createHistory({ hashMode, basename, setPath, onNavigate }),
+        [setPath, basename, hashMode, onNavigate],
+    );
     const matchRoute = useMemo(() => createRouteMatcher(routeMatcherFactory), [routeMatcherFactory]);
     const router = useMemo<RouterContextValue>(
         () => ({
--- packages/router/src/history.ts
+++ packages/router/src/history.ts
@@ -1,15 +1,31 @@
 import { getPathWithoutBasename } from "./basename";
 
+export type OnNavigateData = {
+    from: string;
+    to: string;
+    type: "push" | "replace";
+};
+export type OnNavigateFn = (data: OnNavigateData) => Promise<boolean> | boolean;
+
 export interface RouterHistory {
-    push: (path: string) => void;
-    replace: (path: string) => void;
+    /** Resolves to false if onNavigate resolved to false */
+    push: (path: string) => Promise<boolean>;
+    /** Resolves to false if onNavigate resolved to false */
+    replace: (path: string) => Promise<boolean>;
     onChange: () => void;
     urlTo: (path: string) => string;
 }
 
 export const getHashPath = () => window.location.hash.substr(1);
 
-export function createHistory(hashMode: boolean, basename: string, setPath: (path: string) => void): RouterHistory {
+type CreateHistoryOptions = {
+    hashMode: boolean;
+    basename: string;
+    setPath: (path: string) => void;
+    onNavigate?: OnNavigateFn;
+};
+
+export function createHistory({ hashMode, basename, setPath, onNavigate }: CreateHistoryOptions): RouterHistory {
     const { location, history } = window;
     const onChange = hashMode ? () => setPath(getHashPath()) : () => setPath(getPathWithoutBasename(basename));
 
@@ -17,17 +33,28 @@ export function createHistory(hashMode: boolean, basename: string, setPath: (pat
         ? (path: string) => `${location.pathname}${location.search}#${path}`
         : (path: string) => `${basename}${path}`;
 
+    async function historyAction(path: string, type: "push" | "replace") {
+        const to = urlTo(path);
+        const from = location.pathname + location.search + location.hash;
+        // Only push if something changed.
+        if (to !== from) {
+            if (onNavigate) {
+                const allowed = await onNavigate({ from, to, type: "push" });
+                if (!allowed) return false;
+            }
+
+            if (type === "push") history.pushState({}, "", to);
+            else history.replaceState({}, "", to);
+
+            onChange();
+        }
+
+        return true;
+    }
+
     return {
-        push(path: string) {
-            const newPath = urlTo(path);
-            // Only push if something changed.
-            if (newPath !== location.pathname + location.search + location.hash) history.pushState({}, "", newPath);
-            onChange();
-        },
-        replace(path: string) {
-            history.replaceState({}, "", urlTo(path));
-            onChange();
-        },
+        push: (path: string) => historyAction(path, "push"),
+        replace: (path: string) => historyAction(path, "replace"),
         onChange,
         urlTo,
     };
